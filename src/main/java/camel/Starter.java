0863364c083ce991f7a0e7349676999d5f6ce1e3
cexbrayat <cexbrayat@gmail.com>
2013-03-15 14:16:39 +0100

    startup bootstrap

--- src/main/java/camel/Starter.java
+++ src/main/java/camel/Starter.java
@@ -1,4 +1,28 @@
 package camel;
 
+import org.apache.camel.main.Main;
+
 public class Starter {
+
+    private Main main;
+
+    public static void main(String[] args) throws Exception {
+        Starter example = new Starter();
+        example.boot();
+    }
+
+    public void boot() throws Exception {
+        // create a Main instance
+        main = new Main();
+        // enable hangup support so you can press ctrl + c to terminate the JVM
+        main.enableHangupSupport();
+
+        // add routes
+        //main.addRouteBuilder(new MyRouteBuilder());
+
+        // run until you terminate the JVM
+        System.out.println("Starting Camel. Use ctrl + c to terminate the JVM.\n");
+        main.run();
+    }
+
 }
